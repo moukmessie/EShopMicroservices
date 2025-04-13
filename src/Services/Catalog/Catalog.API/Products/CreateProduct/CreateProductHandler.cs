01846f8266583198c69f807f5bb3a6f8d2552108
moukmessie <moukmessie@gmail.com>
2025-04-13 21:57:31 +0200

    Ajout de nouveaux projets et configuration Docker
    - Ajout des projets "Services", "Catalog", "Catalog.API" et "BuildingBlocks" dans la solution.
    - Mise à jour du fichier `.dockerignore` pour exclure les fichiers non nécessaires.
    - Configuration du SDK .NET 8.0 et ajout de références à `MediatR` dans les fichiers de projet.
    - Implémentation des interfaces de commande et de requête pour le modèle CQRS.
    - Ajout des fichiers de configuration `appsettings.Development.json` et `appsettings.json` pour la journalisation.
    - Création d'un `Dockerfile` pour la construction et la publication de l'application.
    - Initialisation de l'application Web dans `Program.cs`.
    - Modèle `Product.cs` ajouté pour représenter un produit.
    - Gestion de la création de produits avec `CreateProductEndPoint.cs` et `CreateProductHandler.cs`.
    - Mise à jour de `launchSettings.json` pour les profils de lancement.

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, string Description, decimal Price, string ImageFile, List<string> Categories) 
+        : IRequest<CreateProductResult>;
+    public record CreateProductResult(Guid ID);
+    internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+    {
+        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
