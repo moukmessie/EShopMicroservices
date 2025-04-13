99b138c030d9d260f1f2f1a8f12aaa001e2c92d4
moukmessie <moukmessie@gmail.com>
2025-04-13 23:16:23 +0200

    Add Carter package and implement product creation endpoint

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndPoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndPoint.cs
@@ -1,7 +1,26 @@
-﻿namespace Catalog.API.Products.CreateProduct
+﻿
+namespace Catalog.API.Products.CreateProduct
 {
     public record CreateProductRequest(string Name, string Description, decimal Price, string ImageFile, List<string> Categories);
-    public class CreateProductEndPoint
+    public record CreateProductResponse(Guid Id);
+
+    public class CreateProductEndPoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/products", async (CreateProductRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<CreateProductCommand>();
+                var result = await sender.Send(command);
+                var response = result.Adapt<CreateProductResponse>();
+                return Results.Created($"/products/{response.Id}", response);
+            })
+                .WithName("CreateProduct")
+                .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .WithSummary("Create a new product")
+                .WithDescription("Creates a new product");
+                
+        }
     }
 }
