323d29a1a2ad3eb435af77f022a62820f2a8306d
moukmessie <moukmessie@gmail.com>
2025-04-15 20:26:07 +0200

    Add Marten package and configure database connection; update CreateProduct command handler to save products

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,22 +1,13 @@
-﻿using System.Windows.Input;
-using BuildingBlocks.CQRS;
-using Catalog.API.Models;
-using MediatR;
-
-namespace Catalog.API.Products.CreateProduct
+﻿namespace Catalog.API.Products.CreateProduct
 {
     public record CreateProductCommand(string Name, string Description, decimal Price, string ImageFile, List<string> Categories) 
         : ICommand<CreateProductResult>;
     public record CreateProductResult(Guid ID);
-    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    internal class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
-            // TODO: Implement the logic to create a product
-            // creat product entity from the command object
-            // save  to the database (not implemented here)
-            // return the result with the product ID
-
+         
             var product = new Product
             {
                 Name = command.Name,
@@ -25,9 +16,12 @@ namespace Catalog.API.Products.CreateProduct
                 ImageFile = command.ImageFile,
                 Categories = command.Categories
             };
-          
-            return new CreateProductResult(Guid.NewGuid());
 
+            // save the product to the database
+            session.Store(product);
+            await session.SaveChangesAsync(cancellationToken);
+
+            return new CreateProductResult(Guid.NewGuid());
         }
     }
 }
