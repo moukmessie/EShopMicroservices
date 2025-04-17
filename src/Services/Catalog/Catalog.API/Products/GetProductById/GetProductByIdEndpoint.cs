f293d2268dc07a8e833df2d5381e300a19adb2e3
moukmessie <moukmessie@gmail.com>
2025-04-17 15:27:35 +0200

    Feat: add GetProductById endpoint and query handler for retrieving a product by its ID

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
@@ -0,0 +1,27 @@
+﻿namespace Catalog.API.Products.GetProductById
+{
+    ///public record GetProductByIdRequest()
+    public record GetProductByIdResponse(Product Product);
+
+    public class GetProductByIdEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products/{id:guid}", async (Guid id, ISender sender) =>
+            {
+                var result = await sender.Send(new GetProductByIdQuery(id));
+                var response = result.Adapt<GetProductByIdResponse>();
+                return Results.Ok(response);
+            })
+                .WithName("GetProductById")
+                .Produces<GetProductByIdResponse>(StatusCodes.Status200OK)
+                .ProducesProblem(StatusCodes.Status500InternalServerError)
+                .Produces(StatusCodes.Status400BadRequest)
+                .Produces(StatusCodes.Status404NotFound)
+                .WithTags("Products")
+                .WithSummary("Get product by id")
+                .WithDescription("Get product by id");
+        }
+    }
+   
+}
