be29fb3292db7d256a7edda31e5068b927653607
moukmessie <moukmessie@gmail.com>
2025-04-17 15:58:13 +0200

    Feat: implement UpdateProduct endpoint and command handler for updating a product

---
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -0,0 +1,29 @@
+﻿namespace Catalog.API.Products.UpdateProduct
+{
+    public record UpdateProductCommand(Guid Id, string Name, string Description, decimal Price, string ImageFile, List<string> Categories)
+        : ICommand<UpdateProductResult>;
+    public record UpdateProductResult(bool isSuccess);
+    public class UpdateProductCommandHandler(IDocumentSession session, ILogger<UpdateProductCommandHandler> logger)
+        : ICommandHandler<UpdateProductCommand, UpdateProductResult>
+    {
+        public async Task<UpdateProductResult> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("UpdateProductHandler.Handle called with {@Command}", command);
+            var product = await session.LoadAsync<Product>(command.Id, cancellationToken);
+            if (product is null)
+            {
+                throw new ProductNotFoundException(command.Id);
+            }
+            product.Name = command.Name;
+            product.Description = command.Description;
+            product.Price = command.Price;
+            product.ImageFile = command.ImageFile;
+            product.Categories = command.Categories;
+
+            session.Update(product);
+            await session.SaveChangesAsync(cancellationToken);
+
+            return new UpdateProductResult(true);
+        }
+    }
+}
