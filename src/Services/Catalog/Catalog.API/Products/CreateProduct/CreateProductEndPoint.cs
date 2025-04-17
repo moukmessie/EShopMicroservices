223c4ad0ff69d062300f7c49f110dbd4ef25e230
moukmessie <moukmessie@gmail.com>
2025-04-17 23:07:06 +0200

    Feat: add validation rules for CreateProductCommand and log command handling

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndPoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndPoint.cs
@@ -11,8 +11,11 @@ namespace Catalog.API.Products.CreateProduct
             app.MapPost("/products", async (CreateProductRequest request, ISender sender) =>
             {
                 var command = request.Adapt<CreateProductCommand>();
+
                 var result = await sender.Send(command);
+
                 var response = result.Adapt<CreateProductResponse>();
+
                 return Results.Created($"/products/{response.Id}", response);
             })
                 .WithName("CreateProduct")
--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,13 +1,37 @@
-﻿namespace Catalog.API.Products.CreateProduct
+﻿
+namespace Catalog.API.Products.CreateProduct
 {
     public record CreateProductCommand(string Name, string Description, decimal Price, string ImageFile, List<string> Categories) 
         : ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
-    internal class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
+
+    public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+    {
+        public CreateProductCommandValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .WithMessage("Name is required.");
+            RuleFor(x => x.Price)
+                .GreaterThan(0)
+                .WithMessage("Price must be greater than 0.");
+            RuleFor(x => x.ImageFile)
+                .NotEmpty()
+                .WithMessage("Image file is required.");
+            RuleFor(x => x.Categories)
+                .NotEmpty()
+                .WithMessage("At least one category is required.");
+        }
+    }
+
+    internal class CreateProductCommandHandler(IDocumentSession session, ILogger<CreateProductCommandHandler> logger) : 
+        ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
-         
+            
+            logger.LogInformation("CreateProductCommandHandler.Handle called with {@Command}", command);
+
             var product = new Product
             {
                
