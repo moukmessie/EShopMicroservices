5b4d12afa9f98dee36fc0c130f0ffe6e373e2f0d
moukmessie <moukmessie@gmail.com>
2025-04-17 15:27:01 +0200

    Feat: add ProductNotFoundException class for handling product not found errors

---
+++ src/Services/Catalog/Catalog.API/Exceptions/ProductNotFoundException.cs
@@ -0,0 +1,18 @@
+﻿namespace Catalog.API.Exceptions
+{
+    public class ProductNotFoundException : Exception
+    {
+        public ProductNotFoundException() : base("Product not found")
+        {
+        }
+        public ProductNotFoundException(Guid id) : base($"Product with id {id} not found")
+        {
+        }
+        public ProductNotFoundException(string message) : base(message)
+        {
+        }
+        public ProductNotFoundException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
--- src/Services/Catalog/Catalog.API/GlobalUsing.cs
+++ src/Services/Catalog/Catalog.API/GlobalUsing.cs
@@ -1,6 +1,7 @@
 ﻿global using Carter;
 global using MediatR;
 global using Mapster;
+global using Catalog.API.Exceptions;
 global using BuildingBlocks.CQRS;
 global using Catalog.API.Models;
 global using Marten;
